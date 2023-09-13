29f3b7e4b9a18ab1069f46b83bd0528e43861774
Park jungwoo <pjw951230@gmail.com>
2023-09-13 10:23:46 +0900

    Feat: ApiSingleResponse 코드 추가

--- server/src/main/java/com/pathfinder/server/global/response/ApiSingleResponse.java
+++ server/src/main/java/com/pathfinder/server/global/response/ApiSingleResponse.java
@@ -3,6 +3,7 @@ package com.pathfinder.server.global.response;
 import com.pathfinder.server.global.exception.BusinessException;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
 
 @Getter
 @AllArgsConstructor
@@ -19,5 +20,21 @@ public class ApiSingleResponse<T> {
                 exception.getHttpStatus().value(),
                 exception.getErrorCode(),
                 exception.getMessage());
+    }
+
+    public static <T> ApiSingleResponse<T> ok(T data) {
+        return ApiSingleResponse.of(data, HttpStatus.OK);
+    }
+
+    public static <T> ApiSingleResponse<T> of(T data, HttpStatus httpStatus) {
+        return ApiSingleResponse.of(data, httpStatus, httpStatus.getReasonPhrase());
+    }
+
+    public static <T> ApiSingleResponse<T> of(T data, HttpStatus httpStatus, String message) {
+        return new ApiSingleResponse<>(
+                data,
+                httpStatus.value(),
+                httpStatus.name(),
+                message);
     }
 }
